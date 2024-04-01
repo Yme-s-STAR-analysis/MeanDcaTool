cfb4ac3cb1580f5fcedf6bca7cbdfced1df990e6
Yme <yghuang@mails.ccnu.edu.cn>
2024-04-01 16:22:39 +0200

    v2.1: Template header file, modify parameters accordingly

---
+++ MeanDcaParams.h
@@ -0,0 +1,25 @@
+// Put your mean DCA cuts here
+
+#ifndef __MEANDCAPARAMS__
+#define __MEANDCAPARAMS__
+
+namespace DcaPars {
+
+    static constexpr const char* dstName = "19p6";
+
+    static double DcaZUpperPars[3] = {
+        1, 1, 1
+    };
+    static double DcaZLowerPars[3] = {
+        1, 1, 1
+    };
+    static double DcaXYUpperPars[3] = {
+        1, 1, 1
+    };
+    static double DcaXYLowerPars[3] = {
+        1, 1, 1
+    };
+
+} // namespace DcaPars
+
+#endif
